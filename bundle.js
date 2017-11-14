e5810bd2d55f47927779c792ce2c47c616caf309
David <davidgbanks91@gmail.com>
2017-11-13 20:02:11 -0800

    enemy tank shoots playerOne

--- bundle.js
+++ bundle.js
@@ -103,7 +103,7 @@ class Bullet {
         (this.pos[1] + this.radius) >= object.sides.top &&
         (this.pos[1] - this.radius) <= object.sides.bottom
       )) {
-
+        // console.log(object);
         bool = true;
         this.hitsEnemy(object);
         return;
@@ -127,7 +127,7 @@ class Bullet {
     const explosion = new Explosion(this.pos);
     this.game.explosions.push(explosion);
     setTimeout(() => { this.game.remove(explosion); }, this.explosionTimeout);
-    console.log(this.game.tanks);
+    // console.log(this.game.tanks);
   }
 
   draw(ctx) {
@@ -341,7 +341,8 @@ class Tank {
     this.game = game;
     this.moveDirection = [0, 0];
     this.width = 50;
-    this.speed = 5;
+    this.speed = 15;
+    this.cannonSlope = [0, 0];
   }
 
   idEnemies() {
@@ -461,10 +462,10 @@ class Tank {
     this.cannonSlope = slope;
   }
 
-  fire(mousePos) {
+  fire() {
     const bullet = new Bullet({
       game: this.game,
-      owner: this, 
+      owner: this,
       tankPos: this.pos,
       slope: this.cannonSlope
     });
@@ -692,28 +693,46 @@ class EnemyTank extends Tank {
     this.aimX = (this.pos[0] - 35);
     this.aimY = this.pos[1];
 
-    this.sides = this.getSides();
-    // this.enemies = this.idEnemies();
+    this.lineOfFirePoint = [this.aimX, this.aimY];
+    this.pixelsAwayFromCannon = 1;
 
+    this.sides = this.getSides();
 
     this.move(Object.keys(MOVES)[Math.floor(Math.random() * 4)]);
+
+    setInterval(() => {
+      this.whatAmILookingAt();
+    }, 1);
   }
 
   whatAmILookingAt() {
-    // console.log([this.aimX, this.aimY]);
-    // console.log('slope', (this.pos[1] - this.aimY)/(this.pos[0] - this.aimX));
 
-    console.log(slope);
-    console.log(this.cannonSlope);
+    let objects = [].concat(this.game.coverOnly, [this.game.playerOne]);
 
+    objects.forEach(object => {
+      if ((this.lineOfFirePoint[0] >= object.sides.left &&
+          this.lineOfFirePoint[0] <= object.sides.right) &&
+          (this.lineOfFirePoint[1] >= object.sides.top &&
+          this.lineOfFirePoint[1] <= object.sides.bottom)) {
+        if (object === this.game.playerOne) {
+          this.fire();
+        }
+        this.lineOfFirePoint = [this.aimX, this.aimY];
+        this.pixelsAwayFromCannon = 1;
+      }
+    });
 
-
+    this.lineOfFirePoint = [
+      (this.aimX + (this.cannonSlope[0] * this.pixelsMoved)),
+      (this.aimY + (this.cannonSlope[1] * this.pixelsMoved))
+    ];
+    this.pixelsMoved = this.pixelsMoved + 1;
 
   }
 
   move() {
     Object.keys(MOVES)[Math.floor(Math.random() * 4)]
-    this.whatAmILookingAt();
+    // this.whatAmILookingAt();
   }
 
 }
